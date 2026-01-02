f3263126d1c7049b51fa22771cb35ec29d5f0409
文迪 <wendy8wendy@hotmail.com>
2026-01-02 12:02:39 +0800

    1

---
+++ link/5/jscss/filtertable.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+$('table').filterTable({ // apply filterTable to all tables on this page
+inputSelector: '#input-filter' // use the existing input instead of creating a new one
+    });
+});
---
+++ link/wendytools/colour/js/wdcolor01.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.classypicker1').ClassyColor({
+color: '#ed1978',
+colorSpace: 'rgba',
+labels: true,
+displayColor: 'css',
+
+    });
+    $('.classypicker2').ClassyColor({
+color: '#653298',
+colorSpace: 'rgb',
+labels: true,
+displayColor: 'hex'
+    });
+    $('.classypicker3').ClassyColor({
+color: '#ffcc00',
+colorSpace: 'hsla',
+labels: true,
+displayColor: 'css'
+    });
+    $('.classypicker4').ClassyColor({
+color: '#f64747',
+colorSpace: 'hsl',
+labels: true,
+displayColor: 'hex'
+    });
+});
